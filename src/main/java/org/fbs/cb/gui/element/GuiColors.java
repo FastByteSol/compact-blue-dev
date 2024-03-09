0dd3fcf4e5b1e886589cdcdc8101d019ab2db64d
Vitaliylevchuk <vitalik.levchuk.008@gmail.com>
2024-03-09 18:07:18 +0300

    Adding translucent white, translucent black and light blue color

--- src/main/java/org/fbs/cb/gui/element/GuiColors.java
+++ src/main/java/org/fbs/cb/gui/element/GuiColors.java
@@ -1,4 +1,11 @@
 package org.fbs.cb.gui.element;
 
 public class GuiColors {
+
+    private GuiColors(){}
+
+    public static GuiColor TRANSLUCENT_WHITE = new GuiColor(0x80FFFFFF);
+    public static GuiColor TRANSLUCENT_BLACK = new GuiColor(0x80000000);
+    public static GuiColor LIGHT_BLUE = new GuiColor(0xFF05a8ff);
+
 }
