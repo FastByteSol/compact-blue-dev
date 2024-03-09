5806d437070c1fdcde2c047956899fe2ea812fb4
Vitaliylevchuk <vitalik.levchuk.008@gmail.com>
2024-03-09 17:17:03 +0300

    Add classes for static colors

---
+++ src/main/java/org/fbs/cb/gui/element/GuiColor.java
@@ -0,0 +1,14 @@
+package org.fbs.cb.gui.element;
+
+public class GuiColor {
+
+    private final int COLOR;
+
+    public GuiColor(int color){
+        COLOR = color;
+    }
+
+    public int getColor() {
+        return COLOR;
+    }
+}
