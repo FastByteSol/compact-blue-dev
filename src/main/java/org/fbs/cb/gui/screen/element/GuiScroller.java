7bc861ef89ca43f371088147c819d56de5d059e3
Vitaliylevchuk <vitalik.levchuk.008@gmail.com>
2024-03-10 19:36:52 +0300

    Rename Scroller because it's not a gui element, it's a data class

--- src/main/java/org/fbs/cb/gui/screen/element/GuiScroller.java
+++
@@ -1,7 +0,0 @@
-package org.fbs.cb.gui.screen.element;
-
-public class GuiScroller {
-
-    public
-
-}
---
+++ src/main/java/org/fbs/cb/gui/screen/element/Scroller.java
@@ -0,0 +1,24 @@
+package org.fbs.cb.gui.screen.element;
+
+public class Scroller {
+
+    public Scroller(double min, double max){
+        this.max = max;
+        this.min = min;
+    }
+
+    private final double min, max;
+
+    private double progress = 0;
+
+    public double getProgress() {
+        return progress;
+    }
+    public void add(double value){
+        progress = Math.min(max, progress + value);
+    }
+    public void subtract(double value){
+        progress = Math.max(min, progress - value);
+    }
+
+}
